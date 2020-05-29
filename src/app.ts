4f280436aae05035e67c2067c4dc90598d2c6fac
Vu Nguyen Ngoc <ngocvu1807@gmail.com>
2020-05-30 01:21:10 +0700

    final release of drag and drop project

--- src/app.ts
+++ src/app.ts
@@ -64,6 +64,18 @@ class ProjectState extends State<Project> {
     );
 
     this.projects.push(newProject);
+    this.updateListeners();
+  }
+
+  moveProject(projectId: string, newStatus: ProjectStatus) {
+    const project = this.projects.find((prj) => prj.id === projectId);
+    if (project) {
+      project.status = newStatus;
+      this.updateListeners();
+    }
+  }
+
+  private updateListeners() {
     for (const listenerFn of this.listeners) {
       listenerFn(this.projects.slice());
     }
@@ -229,7 +241,15 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement>
       listEl.classList.add('droppable');
     }
   }
-  dropHandler(event: DragEvent) {}
+
+  @autobind
+  dropHandler(event: DragEvent) {
+    const prjId = event.dataTransfer!.getData('text/plain');
+    projectState.moveProject(
+      prjId,
+      this.type === 'active' ? ProjectStatus.Active : ProjectStatus.Finished
+    );
+  }
 
   @autobind
   dragLeaveHandler(_: DragEvent) {
